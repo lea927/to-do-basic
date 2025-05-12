8f86177621680d03dd404262b7b0ae8997886f39
Lea Tinoso <lxthym@icloud.com>
2025-05-12 19:09:33 +0800

    feat: style todo form

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -13,19 +13,27 @@ const ToDoForm = ({onSaveToDo, itemToEdit, onEditToggle, show, onClose}) => {
   return (
     <>
       <div className="fixed inset-0 bg-opacity-10 flex items-center justify-center z-50" onClick={onClose}>
-        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative" onClick={e => e.stopPropagation()}>
-        <button className="absolute top-2 right-2" onClick={onClose}>✕</button>
-          <input
-            type="text"
-            placeholder="Take out trash"
-            onChange={(e) => setTask(e.target.value)}
-            value={task}
-          />
-          <button type="submit" onClick={() => {
-            onSaveToDo(task)
-            setTask('')
-            onEditToggle(false)
-          }}>Save</button>
+        <div className="text-xl bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative" onClick={e => e.stopPropagation()}>
+        <button className="hover:cursor-pointer absolute top-2 right-2" onClick={onClose}>✕</button>
+          <div className='flex justify-between'>
+            <input
+              type="text"
+              placeholder="Take out trash"
+              onChange={(e) => setTask(e.target.value)}
+              value={task}
+              className='p-3'
+            />
+            <button
+              className='text-white bg-blue-600 hover:cursor-pointer py-3 px-4 rounded-md'
+              type="submit"
+              onClick={() => {
+                onSaveToDo(task)
+                setTask('')
+                onEditToggle(false)
+            }}>
+              Save
+            </button>
+          </div>
         </div>
       </div>
     </>
